ba74f224983ba26a512cc79515ac96b5b2adacd2
Vi Po <pi@homedir.eu>
2014-01-20 23:29:02 +0200

    introduce persistence

--- build.sbt
+++ build.sbt
@@ -4,6 +4,10 @@ version := "0.1"
 
 scalaVersion := "2.10.2"
 
+val akkaVersion = "2.3-M2"
+
+val sprayVersion = "1.3-M2"
+
 mainClass := Some("vipo.guess.Main")
 
 resolvers ++= Seq(
@@ -12,10 +16,12 @@ resolvers ++= Seq(
 )
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % "2.2.3",
-  "com.typesafe.akka" %% "akka-slf4j" % "2.2.3",
-  "io.spray" % "spray-can" % "1.2.0",
-  "io.spray" % "spray-routing" % "1.2.0",
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
+  "com.typesafe.akka" %% "akka-persistence-experimental" % akkaVersion,
+  "org.reactivemongo" %% "reactivemongo" % "0.10.0",
+  "io.spray" % "spray-can" % sprayVersion,
+  "io.spray" % "spray-routing" % sprayVersion,
   "ch.qos.logback" % "logback-classic" % "1.0.13",
   "org.scalatest" %% "scalatest" % "2.0" % "test"
 )
--- src/main/scala/vipo/guess/actors/StatisticsActor.scala
+++ src/main/scala/vipo/guess/actors/StatisticsActor.scala
@@ -2,19 +2,42 @@ package vipo.guess.actors
 
 import akka.actor.Actor
 import akka.actor.ActorLogging
-
-import scala.collection.mutable.Map
+import akka.persistence.Processor
+import akka.persistence.Persistent
+import akka.persistence.SnapshotOffer
+import akka.persistence.SaveSnapshotFailure
+import akka.persistence.SaveSnapshotSuccess
 
 case class SampleGenerated(val langNo: Int)
 case class GetSampleGeneratedTimes(val langNo: Int)
 
-class StatisticsActor extends Actor with ActorLogging {
+class StatisticsActor extends Processor with ActorLogging {
 
-  private val langGenerated: Map[Int, Long] = Map().withDefaultValue(0)
-  
+  private type Data = Map[Int, Long]
+  private val snapMessage = "snap"
+  private var langGenerated: Data = Map().withDefaultValue(0)
+
+  override def preStart() = try super.preStart finally scheduleSnapshot
+
   def receive = {
-    case SampleGenerated(no) => langGenerated += (no -> (langGenerated(no) + 1))
+    case Persistent(SampleGenerated(no), _) => langGenerated =
+      langGenerated + (no -> (langGenerated(no) + 1))
+    case SnapshotOffer(metadata, offeredSnapshot) =>
+      try langGenerated = offeredSnapshot.asInstanceOf[Data]
+      finally log.debug(s"Recovered from snapshot: $metadata")
+    case s if s == snapMessage => saveSnapshot(langGenerated)
+    case msg@SampleGenerated(_) => self forward Persistent(msg)
     case GetSampleGeneratedTimes(no) => sender ! langGenerated(no)
-    case _ => log.error("received unknown message")
+    case SaveSnapshotSuccess(metadata) =>
+      try scheduleSnapshot
+      finally log.debug(s"Snapshot saved $metadata")
+    case SaveSnapshotFailure(metadata, reason) => log.error(s"Snapshot $metadata failed", reason)
+    case msg@_ => log.error(s"received unknown message: $msg")
+  }
+
+  private def scheduleSnapshot = {
+    import vipo.guess.Bootstrap._
+    import scala.concurrent.duration._
+    system.scheduler.scheduleOnce(4 hours, self, snapMessage)
   }
 }
