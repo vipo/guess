771696cc2a43889d717a129844179e2a5e90afa7
Vi Po <pi@homedir.eu>
2014-01-23 22:38:11 +0200

    switch from tuple to serializable class

--- src/main/scala/vipo/guess/actors/StatisticsActor.scala
+++ src/main/scala/vipo/guess/actors/StatisticsActor.scala
@@ -16,16 +16,20 @@ case class SampleGenerated(val langNo: LangNo)
 case class GetSampleGeneratedTimes(val langNo: LangNo)
 
 object StatisticsActor {
+  type SampleGenData = Map[LangNo, Long]
+  type ChallengeData = Map[ChallengeId, Long]
   val SnapshotDuration = 10 hours
   val SnapshotDurationInitial = 10 seconds
 }
 
+@SerialVersionUID(42L)
+case class SnapshotData(
+  val sampleGenData: StatisticsActor.SampleGenData,
+  val challengeData: StatisticsActor.ChallengeData) extends Serializable
+
 class StatisticsActor extends Processor with ActorLogging {
   import StatisticsActor._
   
-  private type SampleGenData = Map[LangNo, Long]
-  private type ChallengeData = Map[ChallengeId, Long]
-  private type StatisticsSnapshotData = Tuple2[SampleGenData, ChallengeData]
   private val snapMessage = "snap"
   private var langGenerated: SampleGenData = Map().withDefaultValue(0)
   private var challengesTried: ChallengeData = Map().withDefaultValue(0)
@@ -40,19 +44,16 @@ class StatisticsActor extends Processor with ActorLogging {
     case msg@SampleGenerated(_) => self forward Persistent(msg)
     case GetSampleGeneratedTimes(no) => sender ! langGenerated(no)
     //persistence
-    case SnapshotOffer(metadata, snapshot) if snapshot.isInstanceOf[StatisticsSnapshotData]=> {
-      val s: StatisticsSnapshotData = snapshot.asInstanceOf[StatisticsSnapshotData]
-      langGenerated = s._1
-      challengesTried = s._2
-      log.info(s"Recovered from snapshot: $metadata")
-    }
+    case SnapshotOffer(metadata, SnapshotData(g, c)) =>
+      try {langGenerated = g; challengesTried = c}
+      finally log.info(s"Recovered from snapshot: $metadata")
     case s if s == snapMessage =>
-      saveSnapshot((langGenerated, challengesTried))
+      saveSnapshot(SnapshotData(langGenerated, challengesTried))
     case SaveSnapshotSuccess(metadata) =>
       try scheduleSnapshot()
       finally log.info(s"Snapshot saved $metadata")
     case SaveSnapshotFailure(metadata, reason) =>
-      log.error(s"Snapshot $metadata failed", reason)
+      log.error(s"Snapshot $metadata failed: $reason")
     case msg => log.error(s"received unknown message: $msg")
   }
 
