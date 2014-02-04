e3f0ba8d1868c3cf5f3cb154e6ebf6a2c77ce954
Vi Po <pi@homedir.eu>
2014-02-04 22:02:21 +0200

    introduce master key

--- src/main/scala/vipo/guess/Bootstrap.scala
+++ src/main/scala/vipo/guess/Bootstrap.scala
@@ -1,6 +1,7 @@
 package vipo.guess
 
 import akka.actor.{ActorSystem, Props}
+import vipo.guess.domain.Language.LangNo
 import vipo.guess.actors.RouterActor
 import vipo.guess.actors.StatisticsActor
 import akka.util.Timeout
@@ -9,21 +10,26 @@ import com.typesafe.config.ConfigFactory
 import scala.collection.JavaConversions._
 
 object Bootstrap {
-  
+
   implicit val DefaultTimeout = Timeout(2 seconds)
-    
+
   implicit val System = ActorSystem()
-  
+
   implicit val ExecutionContext = System.dispatcher
 
   val SnapshotDuration = 20 hours
 
   val SnapshotDurationInitial = 4 minutes
+
+  val GuessConfig = System.settings.config.getConfig("guess")
+
+  val MasterKey: String = GuessConfig.getString("master-key")
   
-  val Tokens: List[String] = System.settings.config.getStringList("tokens").toList
-  
+  val Tokens: Map[LangNo, String] = GuessConfig.getStringList("tokens").zipWithIndex.
+  		map({case (k, i) => (i+1, k)}).toMap
+
   val Router = System.actorOf(Props[RouterActor], name = "router")
-  
+
   val Stats = System.actorOf(Props[StatisticsActor], name = "stats")
 
 }
