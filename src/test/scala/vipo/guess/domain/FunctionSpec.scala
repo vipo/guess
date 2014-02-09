616fe2074ca9a3b895ca62edddb0b1990f28e2e5
Vi Po <pi@homedir.eu>
2014-02-09 10:17:49 +0200

    finalize function test

--- src/test/scala/vipo/guess/domain/FunctionSpec.scala
+++ src/test/scala/vipo/guess/domain/FunctionSpec.scala
@@ -3,21 +3,24 @@ package vipo.guess.domain
 import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
 import org.scalatest.FlatSpec
+import org.scalautils.Pass
+import org.scalatest.Matchers
 
 object FunctionSpec {
-  val Tries = 100;
+  val Tries = 10;
 }
 
-class FunctionSpec extends FlatSpec {
+class FunctionSpec extends FlatSpec with Matchers {
   import FunctionSpec._
-  
-  "Any function" should "be printed and parsed back" in {
-  
-    for (lang <- Language.AllLanguages.keySet) {
+    
+  for (lang <- Language.AllLanguages.keySet) {
+    s"Any function of language ${lang}" should "be parsed back and evaluated correctly" in {
       for (_ <- 1 to Tries) {
         val original: Function = Language.randomFunction(lang)._1
         val parsed: Function = Function.parse(original.toString).right.get
-        println(parsed)
+        for (arg <- Language.MinValue to Language.MaxValue) {
+          original(arg) should be (parsed(arg))
+        }
       }
     }
   }
